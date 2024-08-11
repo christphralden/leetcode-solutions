16baa005d9cd08dce7371f83ac5e6bea35d6387c
christopher-alden <christopheralden04@gmail.com>
2024-08-11 23:53:52 +0700

    0217: Time: 55ms (99.46%) | Memory: 61.06 (82.78%)

--- 0217_ContainsDuplicate/solution-2.ts
+++ 0217_ContainsDuplicate/solution-2.ts
@@ -1,17 +1,10 @@
-class Solution {
-public:
-    bool containsDuplicate(vector<int>& nums) {
-      sort(nums.begin(), nums.end());
-      int size = nums.size();
-      for(int i=0;i<size-1;i++){
-        if(nums[i] == nums[i+1]) return true;
-      }
-      return false;
-    };
-};
-auto fastio=[](){
-    std::ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return nullptr;
-}();
+function containsDuplicate(nums: number[]): boolean {
+  const arr: number[] = [];
+  let i = 0;
+  const size = nums.length;
+  while (i < size) {
+    if ((arr[nums[i]] = (arr[nums[i]] ?? 0) + 1) > 1) return true;
+    i++;
+  }
+  return false;
+}
