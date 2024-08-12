692c8e52f084c8e7d73e23ea91a4f6e6701c957e
christopher-alden <christopheralden04@gmail.com>
2024-08-12 20:30:13 +0700

    0058: Time: 39ms (99.51%) | Memory: 51.12 (72.68%)

--- 0058_LengthOfLastWord/solution.ts
+++ 0058_LengthOfLastWord/solution.ts
@@ -1,6 +1,4 @@
-// Time: O(n) | Memory: O(n)
+// Time: 39ms (99.51%) | Memory: 51.12 (72.68%)
 function lengthOfLastWord(s: string): number {
   return s.trim().split(" ").pop()?.length ?? 0;
 }
-
-console.log(lengthOfLastWord("   fly me   to   the moon  "));
