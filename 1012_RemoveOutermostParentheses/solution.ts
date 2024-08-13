862bf2810f89578721b78feb18313335a9eb4ecc
christopher-alden <christopheralden04@gmail.com>
2024-08-13 13:01:09 +0700

    1012: Time: 51ms (93.41%) | Memory: 54.67 (5.49%)

--- 1012_RemoveOutermostParentheses/solution.ts
+++ 1012_RemoveOutermostParentheses/solution.ts
@@ -1,3 +1,4 @@
+// Time: 51ms (93.41%) | Memory: 54.67 (5.49%)
 function removeOuterParentheses(s: string): string {
   const stack: string[] = [];
   const result: string[] = [];
@@ -9,7 +10,6 @@ function removeOuterParentheses(s: string): string {
       }
       stack.push(char);
     } else {
-      // char === ')'
       stack.pop();
       if (stack.length > 0) {
         result.push(char);
